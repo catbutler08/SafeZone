7643d7648a49825a6b15e0c411b05750df54df86
Peulto <61219276+turtle85917@users.noreply.github.com>
2025-07-19 20:44:35 +0900

    feat(FrontEnd): Install `tailwindcss` and `react-router-dom`
    ... decorating main page

---
+++ frontend/src/pages/Main.tsx
@@ -0,0 +1,8 @@
+const Main:React.FC = () => {
+  return (
+    <>
+      <div className="text-2xl font-bold">Hello, World!</div>
+    </>
+  );
+};
+export default Main;
---
+++ frontend/src/router.tsx
@@ -0,0 +1,12 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import Main from "./pages/main";
+
+const Router:React.FC = () => {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  </BrowserRouter>
+};
+export default Router;
